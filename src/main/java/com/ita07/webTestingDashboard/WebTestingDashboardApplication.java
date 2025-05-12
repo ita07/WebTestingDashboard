15db490e15639213cc3f99ca04e673db7fc928fb
Ilias Tapeinos <Iliastapeinos99@gmail.com>
2025-05-12 21:38:55 +0300

    Initialize WebTestingDashboard project with basic structure

---
+++ src/main/java/com/ita07/webTestingDashboard/WebTestingDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.ita07.webTestingDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTestingDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebTestingDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ita07/webTestingDashboard/WebTestingDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ita07.webTestingDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTestingDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
